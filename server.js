6aede9ee3100f22236ac78c7a58d004056b58635
Sreekanth Govindaiah <31440235+sreekanth-bg@users.noreply.github.com>
2019-12-28 13:41:42 +0530

    CURD updates

--- server.js
+++ server.js
@@ -16,16 +16,28 @@ const dbName = "data";
 const collectionName = "movies";
 
 // << db init >>
-db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+db.initialize(dbName, collectionName, function (dbCollection) { // successCallback
     // get all items
-    dbCollection.find().toArray(function(err, result) {
+    dbCollection.find().toArray(function (err, result) {
         if (err) throw err;
-          console.log(result);
+        console.log(result);
     });
 
     // << db CRUD routes >>
 
-}, function(err) { // failureCallback
+    server.post("/items", (request, response) => {
+        const item = request.body;
+        dbCollection.insertOne(item, (error, result) => { // callback of insertOne
+            if (error) throw error;
+            // return updated list
+            dbCollection.find().toArray((_error, _result) => { // callback of find
+                if (_error) throw _error;
+                response.json(_result);
+            });
+        });
+    });
+
+}, function (err) { // failureCallback
     throw (err);
 });
 
