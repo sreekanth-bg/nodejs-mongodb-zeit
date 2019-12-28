d5566e07c968978f08b8db9936799868a9c546ea
sreekanth-bg <sreekanth.bg@gmail.com>
2019-12-28 13:29:45 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+// server.js
+
+const express = require("express");
+const server = express();
+
+const body_parser = require("body-parser");
+
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
+
+const port = 4000;
+
+// << db setup >>
+const db = require("./db");
+const dbName = "data";
+const collectionName = "movies";
+
+// << db init >>
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+    // get all items
+    dbCollection.find().toArray(function(err, result) {
+        if (err) throw err;
+          console.log(result);
+    });
+
+    // << db CRUD routes >>
+
+}, function(err) { // failureCallback
+    throw (err);
+});
+
+
+
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
